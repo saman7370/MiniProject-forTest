abe7d0b1421cfdde71ce6d97634102576b0233d3
saman7370 <mehralisaman8@gmail.com>
2022-05-09 16:55:05 +0430

    Accordion-test

--- js/javascript.js
+++ js/javascript.js
@@ -1,68 +1,15 @@
-const slides = document.querySelector('.slides').children;
-const next = document.querySelector('.next');
-const prev = document.querySelector('.prev');
-const totalSlider = slides.length;
-let duration = 6000;
-let index = 0;
+const title = document.querySelectorAll('.accordion-title');
 
-
-next.onclick = ()=>{
-    slide('next');
-}
-
-prev.onclick = ()=>{
-    slide('prev');
-}
-
-function slide(direction){
-    progress()
-    if(direction == 'next'){
-        if(index == totalSlider - 1){
-            index = 0;
+title.forEach(item =>{
+    item.addEventListener('click', function(){
+        item.classList.toggle('active');
+        const content = item.nextElementSibling;
+        if(content.style.height){
+            content.style.height = null
         }else{
-            index++;
+            content.style.height = content.scrollHeight + "px";
+            content.style.paddingRight = 10 + "px";
+            content.style.paddingLeft = 10 + "px";
         }
-    }
-    if(direction == 'prev'){
-        if(index == 0){
-            index = totalSlider - 1;
-        }else{
-            index--;
-        }
-    }
-
-    clearInterval(timer);
-    timer = setInterval(autoSlider, duration);
-
-
-    for(let i = 0; i < slides.length; i++){
-        slides[i].classList.remove('active');
-    }
-
-    slides[index].classList.add('active');
-}
-
-
-
-function progress(){
-    document.querySelector('.progress').innerHTML = '';
-    const div = document.createElement('div');
-    div.style.width = '0';
-    div.style.height = '5';
-    div.style.position = 'absolute';
-    div.style.top = '0';
-    div.style.left = '0';
-    div.style.backgroundColor = 'red';
-    div.style.animation= 'progress ' + duration/1000 + "s linear";
-    document.querySelector('.progress').appendChild(div);
-}
-
-
-function autoSlider(){
-    slide('next');
-}
-
-let timer = setInterval(autoSlider,duration);
-
-progress();
-
+    })
+})
