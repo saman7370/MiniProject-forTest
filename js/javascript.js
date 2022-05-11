a8492744d30f43be38fec7ae66593c6ded01c103
saman7370 <mehralisaman8@gmail.com>
2022-05-11 14:38:27 +0430

    CountryCode-test

--- js/javascript.js
+++ js/javascript.js
@@ -1,14 +1,4 @@
-const btn = document.querySelector('.search-icon');
-const overlay = document.querySelector('.search-overlay');
-const popup = document.querySelector('.search-popup');
+const country = document.getElementById('country');
 
-btn.addEventListener('click',function(){
-    overlay.classList.add('active');
-    popup.classList.add('active');
-})
-
-overlay.addEventListener('click', function(){
-    overlay.classList.remove('active');
-    popup.classList.remove('active');
-})
-
+country.onchange = function(){
+    document.getElementById('select-country').innerHTML = country.value + " :";}
