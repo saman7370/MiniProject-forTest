f5804c0e0b26b1c209d5a16a67a48d7fbc73a89b
saman7370 <mehralisaman8@gmail.com>
2022-05-11 14:40:51 +0430

    ResponsiveMenu-test

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,15 @@
-const country = document.getElementById('country');
+const toggleMenu = document.querySelector('.toggle-menu');
 
-country.onchange = function(){
-    document.getElementById('select-country').innerHTML = country.value + " :";}
+toggleMenu.addEventListener('click', openNav );
+
+function openNav(){
+    toggleMenu.classList.toggle('active');
+    const nav = document.querySelector('nav');
+    nav.classList.toggle('open');
+
+    if(nav.classList.contains('open')){
+        nav.style.height = nav.scrollHeight + "px"
+    }else{
+        nav.style.height = "0";
+    }
+}
