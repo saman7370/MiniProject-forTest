b31b8b7f750c6a080260235675d7f9d046b32216
saman7370 <mehralisaman8@gmail.com>
2022-05-15 18:15:29 +0430

    Hambergure-Menu

--- js/javascript.js
+++ js/javascript.js
@@ -1,31 +1,9 @@
-const ul = document.querySelector('ul').children;
-const items = document.querySelector('.items').children;
+const icon = document.querySelector('.icon');
+const nav = document.querySelector('nav');
 
-for(let i = 0; i < ul.length; i++){
+icon.addEventListener('click', function(){
 
-    ul[i].onclick = function(){
-     
-    for(let x = 0; x < ul.length; x++){
-        ul[x].classList.remove('active');
-    }
+    icon.classList.toggle('active');
 
-    ul[i].classList.add('active');
-
-    const displayItem = this.getAttribute('data-filter');
-
-    for(let z =0; z < items.length; z++){
-
-        items[z].style.transform = 'scale(0)';
-        setTimeout(() => {
-            items[z].style.display = 'none';
-        }, 500);
-
-        if(items[z].getAttribute('data-category') == displayItem || displayItem == 'all'){
-            items[z].style.transform = 'scale(1)';
-            setTimeout(() => {
-                items[z].style.display = 'block';
-            }, 500);
-        }
-    }
-}
-}
+    nav.classList.toggle('active');
+})
