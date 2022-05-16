ae2a462ecd1405c94f0007b270f5700ef479ab3e
saman7370 <mehralisaman8@gmail.com>
2022-05-16 12:53:50 +0430

    Change-Color-Product

--- js/javascript.js
+++ js/javascript.js
@@ -1,26 +1,16 @@
-const allbox = document.querySelector('.content').children;
-const boxWidth = allbox[0].querySelector('.ineer').offsetWidth;
-const boxHeight = allbox[0].querySelector('.ineer').offsetHeight;
+function changeImges(event){
+    const box = document.querySelector('.box-img').children;
+    let i = 0;
 
-const div = document.createElement('div');
-div.style.position = "absolute";
-div.style.width = boxWidth + "px";
-div.style.height = boxHeight + "px";
-div.style.backgroundColor = "#f50057";
-div.style.transition = "all 1s ease";
-div.style.left = allbox[0].querySelector('.ineer').offsetLeft + 'px';
-div.style.top = allbox[0].querySelector('.ineer').offsetTop + 'px';
-document.querySelector('.header').appendChild(div);
+    for(i = 0; i < box.length; i++){
+        box[i].classList.remove('active');
+    }
 
-for(let i = 0; i < allbox.length; i++){
-    allbox[i].addEventListener('click', function(){
-        const x = this.offsetLeft;
-        const y = this.offsetTop;
-        div.style.left = x + "px";
-        div.style.top = y + 'px';
-        const width = this.offsetWidth;
-        const height = this.offsetHeight;
-        div.style.width = width + "px";
-        div.style.height = height + "px";
-    })
+
+
+    event.classList.add('active');
+
+    document.querySelector('img').src = event.children[0].src;
+    
+
 }
