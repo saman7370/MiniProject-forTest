a064f413402f9dde19ec837f1a63703acc57643e
saman7370 <mehralisaman8@gmail.com>
2022-05-16 12:49:50 +0430

    Hover-Animation

--- js/javascript.js
+++ js/javascript.js
@@ -1,31 +1,26 @@
-const boxWarnning = document.querySelector('.box-warnning');
-const btn = document.querySelector('.btn');
-const close = document.querySelector('.close-btn');
-let timer;
+const allbox = document.querySelector('.content').children;
+const boxWidth = allbox[0].querySelector('.ineer').offsetWidth;
+const boxHeight = allbox[0].querySelector('.ineer').offsetHeight;
 
-btn.addEventListener('click',function(){
-    showAlertBox();
-})
+const div = document.createElement('div');
+div.style.position = "absolute";
+div.style.width = boxWidth + "px";
+div.style.height = boxHeight + "px";
+div.style.backgroundColor = "#f50057";
+div.style.transition = "all 1s ease";
+div.style.left = allbox[0].querySelector('.ineer').offsetLeft + 'px';
+div.style.top = allbox[0].querySelector('.ineer').offsetTop + 'px';
+document.querySelector('.header').appendChild(div);
 
-close.addEventListener('click',function(){
-    hidenAlert();
-    clearTimeout(timer);
-})
-
-function showAlertBox(){
-    boxWarnning.classList.add('show');
-    if(boxWarnning.classList.contains('hiden')){
-        boxWarnning.classList.remove('hiden')
-    }
-    timer = setTimeout(() => {
-        hidenAlert();
-        
-    }, 5000);
-
-}
-
-function hidenAlert(){
-    boxWarnning.classList.remove('show');
-    boxWarnning.classList.add('active');
-
+for(let i = 0; i < allbox.length; i++){
+    allbox[i].addEventListener('click', function(){
+        const x = this.offsetLeft;
+        const y = this.offsetTop;
+        div.style.left = x + "px";
+        div.style.top = y + 'px';
+        const width = this.offsetWidth;
+        const height = this.offsetHeight;
+        div.style.width = width + "px";
+        div.style.height = height + "px";
+    })
 }
