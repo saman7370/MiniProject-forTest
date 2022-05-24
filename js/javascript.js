8041b5da024a63adca4ba29894f11dac66921bb7
saman7370 <mehralisaman8@gmail.com>
2022-05-24 13:56:15 +0430

    Movies-Gallery-test

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,3 @@
-const container = document.querySelector('.container');
-window.onmousemove = function(e){
-    let x = e.clientX / 10;
-    let y = e.clientY / 10;
-    
-    container.style.backgroundPositionX = x + "px";
-    container.style.backgroundPositionY = y + "px";}
+function videoUrl(link){
+    document.getElementById('slider').src = "video/" + link;
+}
